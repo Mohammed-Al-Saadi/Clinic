851e44cda047a18d5d3a38fad438090f2570d184
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2023-05-19 02:58:23 +0300

    created user main route

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ require("dotenv").config({ path: './.env' });
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const user = require("../server/routes/routes");
 
 
 
 app.use(express.json());
 app.use(cors());
+app.use('/user', user)
 
 // Start the server
 const port = process.env.PORT || 3001
