6c25c5e64dd2c77750498e3f1cdb4b4d8521e040
mohamed-aje <chochoaj93@gmail.com>
2023-05-25 17:42:53 +0300

    edited response messages

--- server/controller/login.js
+++ server/controller/login.js
@@ -1,57 +1,57 @@
 const bcrypt = require("bcrypt");
 const pool = require("../config/database");
 const { RefreashToken, AccessToken } = require("../utils/jwtTokens");
-require("dotenv").config({ path: '../.env' });
-
-
+require("dotenv").config({ path: "../.env" });
 
 //Login Function
-exports.login =  async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await pool.query(`SELECT * FROM users WHERE email= $1;`, [email]) //Verifying if the user exists in the database
-        if (user.rows.length === 0) {
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await pool.query(`SELECT * FROM users WHERE email= $1;`, [
+      email,
+    ]); //Verifying if the user exists in the database
+    if (user.rows.length === 0) {
+      res.status(400).json({
+        error: "Invalid username or password",
+      });
+    } else {
+      bcrypt.compare(password, user.rows[0].password, (err, result) => {
+        //Comparing the hashed password
+        if (err) {
+          res.status(500).json({
+            error: "Server error",
+          });
+        } else if (result === true) {
+          //Checking if credentials match
+          const Access_Token = AccessToken(user.rows[0].user_id);
+          const Refresh_Token = RefreashToken(user.rows[0].user_id);
+          const firstName = user.rows[0].first_name;
+          const lastName = user.rows[0].last_name;
+          const fullName = firstName.concat(" ", lastName);
+          try {
+            res.json({
+              fullName,
+              Access_Token,
+              Refresh_Token,
+              message: "Login successful",
+            });
+          } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Server error");
+          }
+        } else {
+          //Declaring the errors
+          if (result != true)
             res.status(400).json({
-                error: "User is not registered, Sign Up first",
+              error: "Password or email is incorrect!",
             });
         }
-        else {
-            bcrypt.compare(password, user.rows[0].password, (err, result) => { //Comparing the hashed password
-                if (err) {
-                    res.status(500).json({
-                        error: "Server error",
-                    });
-                } else if (result === true) { //Checking if credentials match
-                    const Access_Token = AccessToken(user.rows[0].user_id) 
-                    const Refresh_Token = RefreashToken(user.rows[0].user_id) 
-                    const firstName = user.rows[0].first_name
-                    const lastName = user.rows[0].last_name
-                    const fullName= firstName.concat(' ',lastName)
-                    try {
-                        res.json({fullName ,Access_Token, Refresh_Token})
-
-                    } catch (error) {
-
-                        console.error(error.message)
-                        res.status(500).send("Server error")
-                        
-                    }
-                    
-                
-                }
-                else {
-                    //Declaring the errors
-                    if (result != true)
-                        res.status(400).json({
-                            error: "Password or email is incorrect!",
-                        });
-                }
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            error: "Server error!", //Database connection 
-        });
-    };
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "Server error!", //Database connection
+    });
+  }
 };
--- server/controller/register.js
+++ server/controller/register.js
@@ -1,53 +1,53 @@
 const bcrypt = require("bcrypt");
 const pool = require("../config/database");
-require("dotenv").config({ path: '../.env' });
-
+require("dotenv").config({ path: "../.env" });
 
 //Registration Function
 
 exports.register = async (req, res) => {
-    const { first_name, last_name, email, password } = req.body;
-  
-    try {
-        const user = await pool.query(`SELECT * FROM users WHERE email= $1;`, [email]); //Checking if user already exists
-        if (user.rows.length != 0 ) {
-            return res.status(400).json({
-                error: "Email already there, No need to register again.",
-            });
-        }
-        else {
-            bcrypt.hash(password, 10, (err, hash) => {
-                if (err)
-                    res.status(err).json({
-                        error: "Please check your password!!",
-                    });
-                const user = {
-                    first_name,
-                    last_name,
-                    email,
-                    password: hash,
-                };
+  const { first_name, last_name, email, password } = req.body;
 
-                //Inserting data into the database
+  try {
+    const user = await pool.query(`SELECT * FROM users WHERE email= $1;`, [
+      email,
+    ]); //Checking if user already exists
+    if (user.rows.length != 0) {
+      return res.status(400).json({
+        error: "Username already taken.",
+      });
+    } else {
+      bcrypt.hash(password, 10, (err, hash) => {
+        if (err)
+          res.status(err).json({
+            error: "Please check your password!!",
+          });
+        const user = {
+          first_name,
+          last_name,
+          email,
+          password: hash,
+        };
 
-                pool.query(`INSERT INTO users (first_name, last_name, email, password) VALUES ($1,$2,$3,$4);`, [user.first_name, user.last_name, user.email, user.password], (err) => {
+        //Inserting data into the database
 
-                    try {
-                        res.status(200).send({ message: 'User added to database'});
-                    } catch (error) {
-                        console.error(error.message)
-                        res.status(500).send(" server error")   
-                    }
-                })
-           
-            });
-        }
+        pool.query(
+          `INSERT INTO users (first_name, last_name, email, password) VALUES ($1,$2,$3,$4);`,
+          [user.first_name, user.last_name, user.email, user.password],
+          (err) => {
+            try {
+              res.status(200).send({ message: "User registered successfully" });
+            } catch (error) {
+              console.error(error.message);
+              res.status(500).send(" server error");
+            }
+          }
+        );
+      });
     }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            error: "Please check your connection!", //Database connection error
-        });
-    };
-}
-
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "Please check your connection!", //Database connection error
+    });
+  }
+};
