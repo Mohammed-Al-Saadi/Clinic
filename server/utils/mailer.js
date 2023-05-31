80492675f739cd9561a8a795be4d2d639cc492cb
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2023-05-31 20:48:58 +0300

    added nodemailer middleware for sending emails

---
+++ server/utils/mailer.js
@@ -0,0 +1,35 @@
+require("dotenv").config({ path: "./.env" });
+var nodemailer = require("nodemailer");
+
+
+const mailer = (email, text, subject) => {
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.EMAILPSW,
+        },
+      });
+
+      const mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: subject,
+        text: text,
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+
+};
+
+module.exports = mailer;
+
+
+
