fa78a517ac12a825655e8661512de68737667fe3
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2023-05-31 13:23:17 +0300

    added forget-password route to the main

--- server/index.js
+++ server/index.js
@@ -4,11 +4,16 @@ const app = express();
 const cors = require("cors");
 const userRouter = require("./routes/userRouter");
 const protectedrouter = require("./routes/protectedRoute");
+const forgetPassword = require('./routes/forgotPassword')
+
+
 
 app.use(express.json());
 app.use(cors());
 app.use("/users", userRouter);
 app.use("/protected", protectedrouter);
+app.use("/reset-password", forgetPassword);
+
 
 // Start the server
 const port = process.env.PORT || 3001;
