405f5e88d36959f4240106ed575cfc9f0f444e14
mohamed-aje <chochoaj93@gmail.com>
2023-06-05 19:25:32 +0300

    fixed the payload and cleaned the code for authMiddleware,jwtTokens

--- server/utils/jwtTokens.js
+++ server/utils/jwtTokens.js
@@ -5,23 +5,14 @@ module.exports = {
     const payload = {
       user: { id: user_id },
     };
-    return jwt.sign(
-      payload,
-
-      process.env.SECRET_KEY,
-      {
-        expiresIn: "15m",
-      }
-    );
+    return jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: "15m",
+    });
   },
   RefreshToken: (user_id) => {
     const payload = {
       user: { id: user_id },
     };
-    return jwt.sign(
-      payload,
-
-      process.env.SECRET_KEY
-    );
+    return jwt.sign(payload, process.env.SECRET_KEY);
   },
 };
