016fcf34a1695e05ee29f8c96ba724ab91fbbbc8
mohamed-aje <chochoaj93@gmail.com>
2023-06-13 18:19:05 +0300

    added two admin routes for now, also included the in index.js app.use adminRouter

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ const app = express();
 const cors = require("cors");
 const userRouter = require("./routes/userRouter");
 const protectedrouter = require("./routes/protectedRoute");
+const adminRouter = require("./routes/AdminRoutes");
 
 app.use(express.json());
 app.use(cors());
 app.use("/users", userRouter);
+app.use("/", adminRouter);
 app.use("/protected", protectedrouter);
 
 // Start the server
