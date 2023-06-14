1f2f36c6edc6010d51dca165155b5b39c30c5f71
mohamed-aje <chochoaj93@gmail.com>
2023-06-14 18:39:41 +0300

    we no longer need updateToAdmin middleware, instead it is now inside the admin controller

---
+++ server/controllers/adminController.js
@@ -0,0 +1,49 @@
+const pool = require("../config/database");
+
+// Get all users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await pool.query("SELECT * FROM users");
+    res.json(users.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Update user to become an admin
+exports.updateToAdmin = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Check if the user is already an admin
+    const existingRole = await pool.query(
+      "SELECT role_name FROM user_roles WHERE user_id = $1",
+      [id]
+    );
+
+    if (
+      existingRole.rows.length !== 0 &&
+      existingRole.rows[0].role_name === "admin"
+    ) {
+      return res.status(400).json({ error: "User is already an admin" });
+    }
+
+    // Perform the update operation
+    const result = await pool.query(
+      "UPDATE user_roles SET role_id = (SELECT role_id FROM roles WHERE role_name = 'admin'), role_name = 'admin' WHERE user_id = $1",
+      [id]
+    );
+
+    // Check if the update was successful
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Return a success message
+    res.json({ message: "User role updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- server/middlewares/roles.js
+++ server/middlewares/roles.js
@@ -1,41 +1,4 @@
 const pool = require("../config/database");
-const updateToAdmin = async (req, res, next) => {
-  const { id } = req.params;
-  console.log("User ID:", id); // Log the ID parameter for debugging
-
-  try {
-    // Check if the user already has the admin role
-    const userRoleQuery = await pool.query(
-      `SELECT * FROM user_roles 
-       WHERE user_id = $1 AND role_name = 'admin'`,
-      [id]
-    );
-
-    console.log("User Role Query:", userRoleQuery.rows); // Log the userRoleQuery result for debugging
-
-    if (userRoleQuery.rows.length === 0) {
-      // User doesn't have the admin role assigned, so update the role
-      const updateUserRoleQuery = await pool.query(
-        `UPDATE user_roles 
-         SET role_id = (SELECT role_id FROM roles WHERE role_name = 'admin'), 
-             role_name = 'admin'
-         WHERE user_id = $1`,
-        [id]
-      );
-
-      console.log("Update User Role Query:", updateUserRoleQuery); // Log the updateUserRoleQuery result for debugging
-
-      // Return a success message for role update
-      return res.json({ message: "User role updated successfully" });
-    } else {
-      // User already has the admin role assigned
-      return res.json({ message: "User already has the admin role assigned" });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
 const checkUserRole = (role) => {
   return async (req, res, next) => {
     console.log("checkUserRole middleware");
@@ -60,6 +23,5 @@ const checkUserRole = (role) => {
 };
 
 module.exports = {
-  updateToAdmin,
   checkUserRole,
 };
