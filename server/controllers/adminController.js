41008a4a43f337d6bdba98a009269785711fd24f
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2023-06-17 22:33:24 +0300

    added update user profile in admin controller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,4 +1,5 @@
 const pool = require("../config/database");
+const bcrypt = require("bcrypt");
 
 // Get all users
 exports.getAllUsers = async (req, res) => {
@@ -45,5 +46,64 @@ exports.updateToAdmin = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
+  }
+};
+//update user profile info
+exports.updateUserProfile = async (req, res) => {
+  const { id } = req.params;
+  const { first_name, last_name, email } = req.body;
+  try {
+    //check if user in database
+    const checkUser = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (checkUser.rows.length !== 0) {
+      return res.status(400).json({ error: "Email already exists" });
+    } else {
+      const user = await pool.query("select * from users where user_id=$1", [
+        id,
+      ]);
+      if (user.rows.length) {
+        await pool.query(
+          "UPDATE users SET first_name=$1, last_name=$2, email=$3 WHERE user_id=$4 ",
+          [first_name, last_name, email, id]
+        );
+        res.json("User profile has been updated!");
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    res.json("Internal server error");
+  }
+};
+
+exports.updateUserPassword = async (req, res) => {
+  try {
+    //check if password in database
+    const { id } = req.params;
+    const { password } = req.body;
+    const user = await pool.query(
+      "select password from users where user_id=$1",
+      [id]
+    );
+    if (user.rows.length) {
+      return bcrypt.hash(password, 10, (err, hash) => {
+        if (err) {
+          return res.status(err).json({
+            error: "Please check your password!",
+          });
+        } else {
+          pool.query("UPDATE users SET password=$1 WHERE user_id=$2 ", [
+            hash,
+            id,
+          ]);
+          res.json("Password has been updated!");
+        }
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.json("Internal server error");
   }
 };
